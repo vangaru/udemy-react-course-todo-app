3b7aa3131a094d39b6c5c3a4f53a7d674eed7dff
vangaru <ivanenko0607@gmail.com>
2021-10-12 16:12:21 +0300

    feat: ability to finish/unfinish tasks

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './todo-list-item.css'
 
 const TodoListItem = ( { label, important = false } ) => {
 
+    const [isDone, setIsDone] = useState(false);
+
     const listItemStyle ={
         color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
+        fontWeight: important ? 'bold' : 'normal',
+    };
+
+    let classNames = 'todo-list-item';
+    if (isDone) {
+        classNames += ' done';
+    }
+
+    const onLabelClick = () => {
+        setIsDone(!isDone);
     };
 
     return (
-        <span className="todo-list-item" >
+        <span className={classNames} onClick={ onLabelClick }>
             <span className="todo-list-item-label" style={listItemStyle}>{ label }</span>
             <button type="button" className="btn btn-outline-success btn-sm">
                 <i className="fa fa-exclamation" />
