212fb956aed759e57b233246feae6d06f54c2e69
vangaru <ivanenko0607@gmail.com>
2021-10-12 16:27:08 +0300

    fix: remove unused init values importance props in app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,9 +7,9 @@ import React from "react";
 const App = () => {
 
     const todoData = [
-        { label: 'Drink coffee', important: false, id: 1 },
-        { label: 'Make awesome app', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
+        { label: 'Drink coffee', id: 1 },
+        { label: 'Make awesome app', id: 2 },
+        { label: 'Have a lunch', id: 3 }
     ];
 
     return (
